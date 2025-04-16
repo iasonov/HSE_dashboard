Delta calculation fixed

src/update.py
@@ -8,7 +8,7 @@ from col_names import *
 
 def update_sheet(aggregated_data, update_delta=False, history_data=None):
 
-
+    prev_file = "templates/prev_data.csv"
     # define the scope
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
@@ -23,40 +23,36 @@ def update_sheet(aggregated_data, update_delta=False, history_data=None):
 
     # get the first sheet of the Spreadsheet
     dashboard_sales = sheet.get_worksheet(0)
-    # dashboard_simple = sheet.get_worksheet(1)
 
+    print("Гугл-дашборд открыт")
+
+    str_time = datetime.now().strftime("%H:%M")
+    str_date = datetime.now().strftime("%d.%m")
 
     if update_delta:
-        # sheet_prev = sheet.worksheet('prev')
-        # sheet.del_worksheet(sheet_prev)
         # TODO - replace using formulae (not absolute range)
-        prev_leads        = np.array(dashboard_sales.get("J2:J42", value_render_option=ValueRenderOption.unformatted))
-        prev_applications = np.array(dashboard_sales.get("O2:O42", value_render_option=ValueRenderOption.unformatted))
-        aggregated_data[col_leads_delta]        = aggregated_data[col_leads]        - prev_leads[:,0]
-        aggregated_data[col_applications_delta] = aggregated_data[col_applications] - prev_applications[:,0]
+        try:
+            df_prev = pd.read_csv(prev_file)
+            prev_leads = df_prev[col_leads]
+            prev_applications = df_prev[col_applications]
+            print("Данные о лидах с прошлого обновления считаны")
+        except:
+            prev_leads        = np.array(dashboard_sales.get("J2:J42", value_render_option=ValueRenderOption.unformatted))[:,0]
+            prev_applications = np.array(dashboard_sales.get("O2:O42", value_render_option=ValueRenderOption.unformatted))[:,0]
+            print("Данные в " + prev_file + " на локальном диске не найдены, считаю дельту относительно гугл-дашборда")
+        aggregated_data[col_leads_delta]        = aggregated_data[col_leads]        - prev_leads
+        aggregated_data[col_applications_delta] = aggregated_data[col_applications] - prev_applications
+        aggregated_data[[col_leads, col_applications]].to_csv(prev_file)
+        print("Сведения о лидах и регистрациях в ЛК за полнедели обновлены")
     else:
         prev_leads_delta        = np.array(dashboard_sales.get("L2:L42", value_render_option=ValueRenderOption.unformatted))
         prev_applications_delta = np.array(dashboard_sales.get("P2:P42", value_render_option=ValueRenderOption.unformatted))
         aggregated_data[col_leads_delta]        = prev_leads_delta[:,0]
         aggregated_data[col_applications_delta] = prev_applications_delta[:,0]
-#БАКЭКАН. Экономический анализ / Москва / 380301 Экономика / факультет экономических наук / Бакалавриат
+        print("Данные за полнедели не обновляются")
 
-        # sheet_instance.duplicate(1, sheet_instance.id+1, "prev")
-
-    # Clear the worksheet before updating
-    # worksheet.clear()
-
-    # Update the worksheet with the aggregated data
-    # try:
-    # sheet_instance.set_dataframe(aggregated_data, 'A1')
     dashboard_sales.update([aggregated_data.columns.values.tolist()] + aggregated_data.values.tolist())
-    #     #sheet_instance.update([aggregated_data.columns.values.tolist()] + aggregated_data.values.tolist())
-    #     print("Google Spreadsheet updated successfully.")
-    # except:
-    #     aggregated_data.to_excel("dashboard.xlsx")
-    #     print("Error update")
-    str_time = datetime.now().strftime("%H:%M")
-    str_date = datetime.now().strftime("%d.%m")
+    print("Данные в гугл-дашборд записаны")
 
     # TODO replace without absolute cell indexes
     dashboard_sales.update_acell('B43', str_time + ", " + str_date + ".2025") # 2025